056e334a716fe31aafcb62f68afe6ebc903c4dd9
unknown <Lucky@WIN-PUTQLI4TIHF.(none)>
2012-11-22 23:15:36 -0800

    ss

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ef0110ee7efff5b271dad036871ecf89e74a4fcab6f857f4dd21cc8a2dfef6487ee27be5160c6ecccea7a6bf9a5998d2833acb0719f9b9fe070a6db8df4dd9c6'
